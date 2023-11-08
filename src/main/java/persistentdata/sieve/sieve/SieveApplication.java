77e5e0dc9c8b7dfdba26e714288393f21393d98b
Ben Wu <wubin28@gmail.com>
2023-11-08 08:44:46 +0800

    chore: create scaffolding project from start.spring.io

---
+++ src/main/java/persistentdata/sieve/sieve/SieveApplication.java
@@ -0,0 +1,13 @@
+package persistentdata.sieve.sieve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SieveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SieveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/persistentdata/sieve/sieve/SieveApplicationTests.java
@@ -0,0 +1,13 @@
+package persistentdata.sieve.sieve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SieveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
